b72f013547ad96d0549dc5afd3fe58a04fd413a8
TheSameMoonInTheSky <53107527+FrankSFR@users.noreply.github.com>
2020-06-23 22:04:58 +0800

    Add files via upload

---
+++ shuruxing.js
@@ -0,0 +1,15 @@
+
+function click_plus() {
+			document.getElementById("result").innerHTML=Number(document.getElementById("number_one").value)+Number(document.getElementById("number_two").value);
+		}
+function click_minus() {
+			document.getElementById("result").innerHTML=Number(document.getElementById("number_one").value)-Number(document.getElementById("number_two").value);
+
+		}
+function click_times() {
+			document.getElementById("result").innerHTML=Number(document.getElementById("number_one").value)*Number(document.getElementById("number_two").value);
+
+		}
+function click_divide() {
+			document.getElementById("result").innerHTML=Number(document.getElementById("number_one").value)/Number(document.getElementById("number_two").value);
+		}
